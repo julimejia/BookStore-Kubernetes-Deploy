a61efb8522dd00afdc8193e357d2c60fd06aeb68
Julian David Mejia Otalvaro <69063283+julimejia@users.noreply.github.com>
2025-05-12 12:01:46 -0600

    Update delivery.py

--- models/delivery.py
+++ models/delivery.py
@@ -2,6 +2,7 @@ from extensions import db
 
 class DeliveryProvider(db.Model):
     __bind_key__ = 'master'
+    __tablename__ = 'delivery_provider
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     coverage_area = db.Column(db.String(150))
