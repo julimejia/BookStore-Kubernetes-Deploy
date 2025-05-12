34ae35769fb120f35e3b5830b5c96e9065ce6c9b
Julian David Mejia Otalvaro <69063283+julimejia@users.noreply.github.com>
2025-05-12 12:04:39 -0600

    Update delivery_assignment.py

--- models/delivery_assignment.py
+++ models/delivery_assignment.py
@@ -1,6 +1,7 @@
 from extensions import db
 
 class DeliveryAssignment(db.Model):
+    __tablename__ = 'delivery_assignment'
     id = db.Column(db.Integer, primary_key=True)
     purchase_id = db.Column(db.Integer, db.ForeignKey('purchase.id'))
     provider_id = db.Column(db.Integer, db.ForeignKey('delivery_provider.id'))
