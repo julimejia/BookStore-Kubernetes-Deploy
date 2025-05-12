7024ca19157c352a24250c3c0ef5748d77f6d874
Julian David Mejia Otalvaro <69063283+julimejia@users.noreply.github.com>
2025-05-12 12:56:30 -0600

    Update purchase.py

--- models/purchase.py
+++ models/purchase.py
@@ -1,6 +1,7 @@
 from extensions import db
 
 class Purchase(db.Model):
+    __bind_key__ = 'master'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
