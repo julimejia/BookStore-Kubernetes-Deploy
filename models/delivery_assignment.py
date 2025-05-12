5da740b688645e32560123438b1f5a63a0022af4
Julian David Mejia Otalvaro <69063283+julimejia@users.noreply.github.com>
2025-05-12 12:45:22 -0600

    Update delivery_assignment.py

--- models/delivery_assignment.py
+++ models/delivery_assignment.py
@@ -1,12 +1,6 @@
 from extensions import db
 
 class DeliveryAssignment(db.Model):
-    __tablename__ = 'delivery_assignment'
     id = db.Column(db.Integer, primary_key=True)
     purchase_id = db.Column(db.Integer, db.ForeignKey('purchase.id'))
     provider_id = db.Column(db.Integer, db.ForeignKey('delivery_provider.id'))
-
-
-      # Relationships
-    provider = db.relationship('DeliveryProvider', back_populates='assignments')
-    purchase = db.relationship('Purchase', back_populates='delivery_assignments')
